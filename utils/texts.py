abb1182000a30aeb040b886de736dfc0f174f4f6
xenion <akumaru.senju@gmail.com>
2022-11-04 20:54:17 +0500

    done: guide text

--- utils/texts.py
+++ utils/texts.py
@@ -1,8 +1,26 @@
 version = "0.1"
 author = 'akumaru'
+from .colors import colors
 
 xodo_text = f"""xodo - CLI tool that helps to follow TODO.
-version= {version}
+version: {version}
 author: {author}
 
 guide -> xodo guide"""
+
+guide_text = f"""{colors.OKCYAN}xodo - CLI tool that helps to follow TODO.
+version:  {version}
+author: {author}
+
+Available commands:
+			{colors.HEADER}add:
+				usage: xodo add
+				to add a new xodo.
+
+			guide:
+				usage: xodo guide
+				to get a guide
+
+			list:
+				usage: xodo list
+				to get all xodos list"""
