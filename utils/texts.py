4db74eb028b635cb206c0f894d8e7b5eb91f77ad
xenion <akumaru.senju@gmail.com>
2022-11-04 21:33:37 +0500

    fixing texts

--- utils/texts.py
+++ utils/texts.py
@@ -2,25 +2,25 @@ version = "0.1"
 author = 'akumaru'
 from .colors import colors
 
-xodo_text = f"""xodo - CLI tool that helps to follow TODO.
+xodo_text = f"""xodus - CLI tool that helps to follow TODO.
 version: {version}
 author: {author}
 
-guide -> xodo guide"""
+guide -> xodus guide"""
 
-guide_text = f"""{colors.OKCYAN}xodo - CLI tool that helps to follow TODO.
+guide_text = f"""{colors.OKCYAN}xodus - CLI tool that helps to follow TODO.
 version:  {version}
 author: {author}
 
 Available commands:
 			{colors.HEADER}add:
-				usage: xodo add
+				usage: xodus add
 				to add a new xodo.
 
 			guide:
-				usage: xodo guide
+				usage: xodus guide
 				to get a guide
 
 			list:
-				usage: xodo list
+				usage: xodus list
 				to get all xodos list"""
